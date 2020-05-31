5efc0c0b895c1cc4f2b8f1464d03df341daf4870
Valer09 <marchitelli.valerio@gmail.com>
2020-05-31 18:22:41 +0200

    Update package.json and server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static file form the dis directory
-app.use(express.static(__dirname + './dist/UMS'));
+app.use(express.static(${__dirname} + './dist/UMS'));
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/UMS/index.hmtl'));
+  res.sendFile(path.join(${__dirname} + '/dist/UMS/index.hmtl'));
 });
 
 // Start the app by listening on the default Heroku port
