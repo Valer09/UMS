d1dc4d1f81dcc9e65b2b75a7f6c26ee67a037403
Valer09 <marchitelli.valerio@gmail.com>
2020-06-01 13:58:45 +0200

    Environment.prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   APIURL: 'https://radiant-thicket-46420.herokuapp.com/users',
-  APIAUTH: 'https://radiant-thicket-46420.herokuapp.com//api/auth/'
+  APIAUTH: 'https://radiant-thicket-46420.herokuapp.com/api/auth/'
 };
